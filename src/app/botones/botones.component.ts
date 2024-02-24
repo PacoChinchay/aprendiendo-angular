fad93e0afc2b74b7ff21b5d96800f628d694c7ae
PacoChinchay <pacochinchay350@gmail.com>
2024-02-23 23:07:46 -0500

    manejando enlace de propiedades

--- src/app/botones/botones.component.ts
+++ src/app/botones/botones.component.ts
@@ -10,5 +10,21 @@ export class BotonesComponent {
 
   text_color: string = ''
 
-  btn_disable: boolean = false
+  btn_color: boolean = false
+
+  btn_disabled: boolean = false
+
+  src: string = ''
+
+  ChangeSrc(btn: string) {
+    
+    const src1: string = "https://images.pexels.com/photos/9248672/pexels-photo-9248672.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    
+    const src2: string = 'https://images.pexels.com/photos/19648032/pexels-photo-19648032/free-photo-of-paisaje-montanas-arboles-otono.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+
+    if (btn === 'btn-1') this.src = src1
+
+    if (btn === 'btn-2') this.src = src2
+
+  }
 }
